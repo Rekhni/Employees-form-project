110f019e492f2f4c36634dc9f29a3771f2cb1c27
Rakhymzhan <mark.john.99@mail.ru>
2024-06-18 11:45:27 -0700

    elements and components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello World!</h2>;
+}
+
+// const Field = () => {
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px'
+//   }
+//   return <input 
+//           placeholder={holder} 
+//           type='Text' 
+//           style={styledField}/>
+// }
+
+class Field extends React.Component {
+    render() {
+        const holder = 'Enter here';
+        const styledField = {
+        width: '300px'
+        }
+        
+        return <input 
+                placeholder={holder} 
+                type='Text' 
+                style={styledField}/>
+    }
+}
+
+
+function Btn() {
+   const txt = 'Log in';
+   let logged = false;
+   return <button>{logged ? 'Enter' : txt}</button>;
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header />
+      <Field />
+      <Btn />
     </div>
   );
 }
 
+export {Header};
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Header } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const text = 'Hello World!';
 
-const elem = (
-  <div>
-      <h2 className="text">Text: {text}</h2>
-      <input type="text"/>
-      <label htmlFor=""></label>
-      <button tabIndex="0">Click</button>
-  </div>
-)
+// const elem = (
+//   <div>
+//       <h2 className="text">Text: {text}</h2>
+//       <input type="text"/>
+//       <label htmlFor=""></label>
+//       <button tabIndex="0">Click</button>
+//   </div>
+// )
 
 // const elem = React.createElement('h2', {className: 'greetings'}, 'Hello Reha!');
 
 root.render(
-  elem
+  <App />
 );
 
 
