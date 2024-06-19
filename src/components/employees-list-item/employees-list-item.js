b597d855371134bcc124a8e9b0344c549addb6be
Rakhymzhan <mark.john.99@mail.ru>
2024-06-18 20:29:02 -0700

    using states in project

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,30 +1,61 @@
+import { Component } from 'react';
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name, salary, isIncrease}) => {
-
-    let classNames = "list-group-item d-flex justify-content-between";
-    if (isIncrease) {
-        classNames += " increase"
+class EmployeesListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isIncrease: false,
+            rise: false
+        }
     }
 
-    return (
-        <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
-            <div className="d-flex justify-content-center align-items-center">
-                <button type="button" 
-                        className="btn-cookie btn-sm">
-                        <i className="fas fa-cookie"></i>
-                </button>
+    onIncrease = () => {
+        this.setState(({isIncrease}) => ({
+            isIncrease : !isIncrease
+        }));
+    }
 
-                <button type="button" 
-                        className="btn-trash btn-sm">
-                        <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    onRise = () => {
+        this.setState(({rise}) => ({
+            rise: !rise
+        }))
+    }
+
+    render() {
+        const {name, salary} = this.props;
+        const {isIncrease, rise} = this.state;
+        
+        let classNames = "list-group-item d-flex justify-content-between";
+        if (isIncrease) {
+            classNames += " increase"
+        }
+
+        if (rise) {
+            classNames += " like";
+        }
+
+    
+        return (
+            <li className={classNames}>
+                <span onClick={this.onRise} className="list-group-item-label">{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button type="button" 
+                            className="btn-cookie btn-sm"
+                            onClick={this.onIncrease}>
+                            <i className="fas fa-cookie"></i>
+                    </button>
+    
+                    <button type="button" 
+                            className="btn-trash btn-sm">
+                            <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployeesListItem;
