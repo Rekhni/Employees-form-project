c16fe68fd2a2e0ad8feff2d88e423b5fb2289283
Rakhymzhan <mark.john.99@mail.ru>
2024-06-18 23:43:23 -0700

    adding employee

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,16 +17,34 @@ class App extends Component {
                 {name: "Omirzhan", salary: 3000, isIncrease: false, id: 2},
                 {name: "Zhanar", salary: 25000, isIncrease: false, id: 3}
             ]
-        }
+        };
+        this.maxId = 4;
     }
 
     deleteItem = (id) => {
+
         this.setState(({data}) => {
 
             return {
                 data: data.filter(item => item.id !== id)
             }
         })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
     }
 
     render() {
@@ -43,7 +61,7 @@ class App extends Component {
                 <EmployeesList 
                 data={this.state.data}
                 onDelete={this.deleteItem}/>
-                <EmployeesAddForm/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         )
     }
