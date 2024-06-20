9dd53e3b90e2c2d377954eb9f91fc237a69dc564
Rakhymzhan <mark.john.99@mail.ru>
2024-06-20 11:56:52 -0700

    Updates

---
+++ precache-manifest.aecea8f77caf25f9c1402d60265227ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403062517aac0aa96faf5b88ef6f5cd6",
+    "url": "/Employees-form-project/index.html"
+  },
+  {
+    "revision": "74047a7d0b3b5a85e914",
+    "url": "/Employees-form-project/static/css/main.443142e8.chunk.css"
+  },
+  {
+    "revision": "e67754bdf9ffbb057920",
+    "url": "/Employees-form-project/static/js/2.af68fada.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Employees-form-project/static/js/2.af68fada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74047a7d0b3b5a85e914",
+    "url": "/Employees-form-project/static/js/main.4f4b9daa.chunk.js"
+  },
+  {
+    "revision": "ae2660d7411a443cc57a",
+    "url": "/Employees-form-project/static/js/runtime-main.45d6e8b3.js"
+  }
+]);
